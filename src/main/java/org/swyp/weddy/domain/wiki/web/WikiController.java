1bfbba3925723e1d30ace85a3bef507ff63deecb
sehoon.gim <sehoongim@gmail.com>
2025-01-30 19:20:05 +0900

    chore: 로그 설정 추가한다
     - 라이브러리 추가(logback 라이브러리는 이미 포함되어 있다)
     - 로그 형식 설정

--- src/main/java/org/swyp/weddy/domain/wiki/web/WikiController.java
+++ src/main/java/org/swyp/weddy/domain/wiki/web/WikiController.java
@@ -1,5 +1,6 @@
 package org.swyp.weddy.domain.wiki.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.swyp.weddy.domain.wiki.service.WikiService;
 import org.swyp.weddy.domain.wiki.service.dto.WikiDto;
 import org.swyp.weddy.domain.wiki.web.response.WikiResponse;
 
+@Slf4j
 @RestController
 @RequestMapping("/wiki")
 public class WikiController {
@@ -21,6 +23,9 @@ public class WikiController {
 
     @GetMapping
     public ResponseEntity<WikiResponse> getWiki(@RequestParam(name = "title") String title) {
+        log.info("로그 메시지");
+        log.error("에러 메시지");
+
         WikiResponse wikiResponse = wikiService.findWiki(WikiDto.from(title));
         return ResponseEntity.ok().body(wikiResponse);
     }
