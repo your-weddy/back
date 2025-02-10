2e94c471f0680bf8ebbfc538cf140171fc3aab0f
metta0 <jiterin908@gmail.com>
2025-02-10 15:18:04 +0900

    refactor: 테스트용 코드 제거

--- src/main/java/org/swyp/weddy/domain/auth/web/AuthController.java
+++ src/main/java/org/swyp/weddy/domain/auth/web/AuthController.java
@@ -5,10 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -59,7 +56,7 @@ public class AuthController {
         }
     }
 
-    @GetMapping("/userinfo")
+    /*@GetMapping("/userinfo")
     public ResponseEntity<String> getUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -71,6 +68,6 @@ public class AuthController {
         Map<String, Object> principal = (Map<String, Object>) authentication.getPrincipal();
 
         return ResponseEntity.ok("ok");
-    }
+    }*/
 
 }
