2684074d1d386aabf0e83b8d7a9efaf96b37f072
sehoon.gim <sehoongim@gmail.com>
2025-02-11 16:26:48 +0900

    test: 불필요한 클래스 삭제한다
     - 앞서 FakeLargeCatService를 정의하고, 여기에 LargeCatServiceImpl에
       정의할 메서드 본문을 미리 정의하는 식으로 테스트를 작성했다
     - 변경이 생길 경우, FakeLargeCatService와 LargeCatServiceImpl 둘 모두에
       변경 사항을 반영해야 하기 때문에 작업이 늘어날 것이 예상된다. 따라서
       LargeCatService에 대한 테스트용 객체를 만들지 않는 게 더 나아 보인다
     - 그래서 FakeLargeCatService를 삭제한다

--- src/test/java/org/swyp/weddy/domain/checklist/service/LargeCatServiceTest.java
+++ src/test/java/org/swyp/weddy/domain/checklist/service/LargeCatServiceTest.java
@@ -3,7 +3,6 @@ package org.swyp.weddy.domain.checklist.service;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.swyp.weddy.common.exception.ErrorCode;
 import org.swyp.weddy.domain.checklist.dao.LargeCatMapper;
 import org.swyp.weddy.domain.checklist.entity.LargeCatItem;
 import org.swyp.weddy.domain.checklist.exception.LargeCatItemNotExistsException;
@@ -16,7 +15,7 @@ class LargeCatServiceTest {
     @DisplayName("대분류 항목 하나를 가져올 수 있다")
     @Test
     public void find_one_large_cat_item() {
-        LargeCatService largeCatService = new FakeLargeCatService(new FakeLargeCatMapper());
+        LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
         LargeCatItemResponse item = largeCatService.findItem(1L, 1L);
         assertThat(item).isNotNull();
     }
@@ -24,27 +23,10 @@ class LargeCatServiceTest {
     @DisplayName("주어진 대분류 아이디에 등록된 항목이 없을 경우를 처리할 수 있다")
     @Test
     public void no_large_cat_item_for_given_id() {
-        LargeCatService largeCatService = new FakeLargeCatService(new FakeLargeCatMapper());
+        LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
         Assertions.assertThrows(LargeCatItemNotExistsException.class, () ->
                 largeCatService.findItem(-1L, 1L)
         );
-    }
-
-    private static class FakeLargeCatService implements LargeCatService {
-        private final LargeCatMapper mapper;
-
-        public FakeLargeCatService(LargeCatMapper mapper) {
-            this.mapper = mapper;
-        }
-
-        @Override
-        public LargeCatItemResponse findItem(Long checklistId, Long id) {
-            LargeCatItem largeCatItem = mapper.selectItem(checklistId, id);
-            if (largeCatItem == null) {
-                throw new LargeCatItemNotExistsException(ErrorCode.NOT_EXISTS);
-            }
-            return LargeCatItemResponse.from(largeCatItem);
-        }
     }
 
     private static class FakeLargeCatMapper implements LargeCatMapper {
