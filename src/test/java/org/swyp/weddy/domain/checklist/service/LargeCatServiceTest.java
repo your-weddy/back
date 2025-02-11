08bd86af6221dee4641024322da6d14e96fd142e
sehoon.gim <sehoongim@gmail.com>
2025-02-11 22:40:24 +0900

    test: 테스트 정리한다
     - 관련 테스트를 하나로 묶는다

--- src/test/java/org/swyp/weddy/domain/checklist/service/LargeCatServiceTest.java
+++ src/test/java/org/swyp/weddy/domain/checklist/service/LargeCatServiceTest.java
@@ -2,6 +2,7 @@ package org.swyp.weddy.domain.checklist.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.swyp.weddy.domain.checklist.dao.LargeCatMapper;
 import org.swyp.weddy.domain.checklist.entity.LargeCatItem;
@@ -12,21 +13,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LargeCatServiceTest {
 
-    @DisplayName("대분류 항목 하나를 가져올 수 있다")
-    @Test
-    public void find_one_large_cat_item() {
-        LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
-        LargeCatItemResponse item = largeCatService.findItem(1L, 1L);
-        assertThat(item).isNotNull();
-    }
+    @DisplayName("findItem()")
+    @Nested
+    class findItemTest {
+        @DisplayName("대분류 항목 하나를 가져올 수 있다")
+        @Test
+        public void find_one_large_cat_item() {
+            LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
+            LargeCatItemResponse item = largeCatService.findItem(1L, 1L);
+            assertThat(item).isNotNull();
+        }
 
-    @DisplayName("주어진 대분류 아이디에 등록된 항목이 없을 경우를 처리할 수 있다")
-    @Test
-    public void no_large_cat_item_for_given_id() {
-        LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
-        Assertions.assertThrows(LargeCatItemNotExistsException.class, () ->
-                largeCatService.findItem(-1L, 1L)
-        );
+        @DisplayName("주어진 대분류 아이디에 등록된 항목이 없을 경우를 처리할 수 있다")
+        @Test
+        public void no_large_cat_item_for_given_id() {
+            LargeCatService largeCatService = new LargeCatServiceImpl(new FakeLargeCatMapper());
+            Assertions.assertThrows(LargeCatItemNotExistsException.class, () ->
+                    largeCatService.findItem(-1L, 1L)
+            );
+        }
     }
 
     private static class FakeLargeCatMapper implements LargeCatMapper {
