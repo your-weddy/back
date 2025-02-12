c12d3accb988154a11515ceea3bda27f36c74dfd
sehoon.gim <sehoongim@gmail.com>
2025-02-12 18:40:00 +0900

    fix: UriFilter.isSkipUri()에서 glob pattern을 지원하도록 바꾼다
     - 기존 isSkipUri()는 glob pattern을 지원하지 않기 때문에 필터를 무시할
       uri를 모두 적어줘야 한다
     - isSkipUri()에 glob pattern을 쓸 수 있도록 바꿔서 간단히 설정할 수
       있도록 한다

--- src/main/java/org/swyp/weddy/common/config/UriFilter.java
+++ src/main/java/org/swyp/weddy/common/config/UriFilter.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.List;
 @Slf4j
 @Component
 public class UriFilter {
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
     @Value("${skip.uris}")
     private String skipUris;
@@ -24,12 +27,7 @@ public class UriFilter {
 
     public boolean isSkipUri(String uri) {
         return skipUriList.stream()
-                .anyMatch(skipUri -> {
-                    // 루트 경로(/) 특별 처리
-                    if (skipUri.trim().equals("/")) {
-                        return uri.equals("/");
-                    }
-                    return uri.startsWith(skipUri.trim());
-                });
+                .map(String::trim)
+                .anyMatch(skipUri -> pathMatcher.match(skipUri, uri));
     }
 }
