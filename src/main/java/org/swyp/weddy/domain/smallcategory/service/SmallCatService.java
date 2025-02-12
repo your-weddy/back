a3cc559a3f924e847f2781ca22908d94b498e53f
metta0 <jiterin908@gmail.com>
2025-02-12 14:26:08 +0900

    refactor: 클래스명 변경

--- src/main/java/org/swyp/weddy/domain/smallcategory/service/SmallCatService.java
+++ src/main/java/org/swyp/weddy/domain/smallcategory/service/SmallCatService.java
@@ -5,7 +5,7 @@ import org.swyp.weddy.domain.smallcategory.web.res.SmallCatItemResponse;
 
 import java.util.List;
 
-public interface SmallCatItemService {
+public interface SmallCatService {
     //내부용 인터페이스
     List<SmallCatItemResponse> findItems(Long checkListId, Long largeCatItemId); //Long checklistId, Long largeCatId);
     boolean deleteAll(Long checkListId, Long largeCatItemId);  //Long checklistId, Long largeCatId,
