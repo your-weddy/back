05b8f56970b559d8702fb1aa9cf322577a6fe581
metta0 <jiterin908@gmail.com>
2025-02-12 14:31:17 +0900

    feat: 소분류 항목 여러개를 가져올 수 있다

---
+++ src/main/java/org/swyp/weddy/domain/smallcategory/service/SmallCatServiceImpl.java
@@ -0,0 +1,67 @@
+package org.swyp.weddy.domain.smallcategory.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.swyp.weddy.domain.smallcategory.dao.SmallCatItemMapper;
+import org.swyp.weddy.domain.smallcategory.entity.SmallCatItem;
+import org.swyp.weddy.domain.smallcategory.service.dto.SmallCatItemDto;
+import org.swyp.weddy.domain.smallcategory.web.res.SmallCatItemResponse;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class SmallCatServiceImpl implements SmallCatService {
+
+    private final SmallCatItemMapper mapper;
+
+    public SmallCatServiceImpl(SmallCatItemMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<SmallCatItemResponse> findItems(Long checkListId, Long largeCatItemId) {
+        log.debug("==========getList 2======== checkListId: "+checkListId+ ", largeCatItemId " + largeCatItemId  );
+        List<SmallCatItem> smallCatItems = mapper.selectAllItems(checkListId, largeCatItemId);
+        List<SmallCatItemResponse> smallCatItemResponses = SmallCatItemResponse.from(smallCatItems);
+        return smallCatItemResponses;
+    }
+
+    @Override
+    public boolean deleteAll(Long checkListId, Long largeCatItemId) {
+        int deletedRows = mapper.deleteAllItems(largeCatItemId);
+        return deletedRows < 0;
+    }
+
+    @Transactional
+    @Override
+    public Long assignItem(SmallCatItemDto dto) {
+        SmallCatItem smallCatItem = SmallCatItem.from(dto);
+        Long smallCatItemId = mapper.insertItem(smallCatItem);
+        return smallCatItemId;
+    }
+
+    @Override
+    public SmallCatItemResponse findDetails(SmallCatItemDto dto) {
+        Long largeCatItemId = dto.getLargeCatItemId();
+        Long smallCatItemId = dto.getId();
+        SmallCatItem smallCatItem = mapper.selectItem(largeCatItemId, smallCatItemId);
+        return SmallCatItemResponse.from(smallCatItem);
+    }
+
+    @Transactional
+    @Override
+    public boolean edit(SmallCatItemDto dto) {
+        SmallCatItem smallCatItem = SmallCatItem.from(dto);
+        int updatedRows = mapper.updateItem(smallCatItem);
+        return updatedRows > 0;
+    }
+
+    @Override
+    public boolean delete(SmallCatItemDto dto) {
+        Long smallCatItemId = dto.getId();
+        int deletedRows = mapper.deleteItem(smallCatItemId);
+        return deletedRows > 0;
+    }
+}
---
+++ src/main/java/org/swyp/weddy/domain/smallcategory/web/SmallCatItemController.java
@@ -0,0 +1,36 @@
+package org.swyp.weddy.domain.smallcategory.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.swyp.weddy.domain.smallcategory.service.SmallCatService;
+import org.swyp.weddy.domain.smallcategory.web.res.SmallCatItemResponse;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("checklist/large-cat-item/small-cat-Item")
+public class SmallCatItemController {
+
+    private final SmallCatService smallCatService;
+
+    public SmallCatItemController(SmallCatService smallCatService) {
+        this.smallCatService = smallCatService;
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<SmallCatItemResponse>> getSmallCatItemList(@RequestParam(name = "checkListId") String checkListId,
+                                                                    @RequestParam(name = "largeCatItemId") String largeCatItemId) {
+        log.debug("==========getList 1======== checkListId: "+checkListId+ ", largeCatItemId " + largeCatItemId  );
+        List<SmallCatItemResponse> items = smallCatService.findItems(Long.valueOf(checkListId), Long.valueOf(largeCatItemId));
+        for (SmallCatItemResponse item : items) {
+            log.debug("==========getList 3======== SmallCatItemResponse:" + item );
+        }
+        return ResponseEntity.ok().body(items);
+    }
+
+}
