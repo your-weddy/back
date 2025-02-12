7d989ef58a34c1167d12ec7e683e6ddefc539bbb
metta0 <jiterin908@gmail.com>
2025-02-12 14:13:32 +0900

    feat: swagger라이브러리 추가

--- src/main/java/org/swyp/weddy/domain/smallcategory/service/SmallCatService.java
+++ src/main/java/org/swyp/weddy/domain/smallcategory/service/SmallCatService.java
@@ -5,15 +5,15 @@ import org.swyp.weddy.domain.smallcategory.web.res.SmallCatItemResponse;
 
 import java.util.List;
 
-public interface SmallCatService {
+public interface SmallCatItemService {
     //내부용 인터페이스
     List<SmallCatItemResponse> findItems(Long checkListId, Long largeCatItemId); //Long checklistId, Long largeCatId);
+    boolean deleteAll(Long checkListId, Long largeCatItemId);  //Long checklistId, Long largeCatId,
+
     //외부용 인터페이스
     SmallCatItemResponse findDetails(SmallCatItemDto dto); //Long checklistId, Long largeCatId, Long smallCatId);
-
     Long assignItem(SmallCatItemDto dto);//Long checklistId, Long largeCatId
-    void edit(SmallCatItemDto dto); //Long checklistId, Long largeCatId, Long smallCatId);
-    void delete(SmallCatItemDto dto); //Long checklistId, Long largeCatId, Long smallCatId);
-    void deleteAll(SmallCatItemDto dto);  //Long checklistId, Long largeCatId,
+    boolean edit(SmallCatItemDto dto); //Long checklistId, Long largeCatId, Long smallCatId);
+    boolean delete(SmallCatItemDto dto); //Long checklistId, Long largeCatId, Long smallCatId);
 
 }
