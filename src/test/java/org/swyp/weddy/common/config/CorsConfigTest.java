7d7d5f67a1b202726a66abef944453af9420ed5a
sehoon.gim <sehoongim@gmail.com>
2025-02-17 00:15:35 +0900

    test: cors 설정 객체 생성을 테스트한다

---
+++ src/test/java/org/swyp/weddy/common/config/CorsConfigTest.java
@@ -0,0 +1,19 @@
+package org.swyp.weddy.common.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CorsConfigTest {
+
+    @DisplayName("cors 설정에 필요한 객체를 생성할 수 있다")
+    @Test
+    public void make_cors_config_bean() {
+        CorsConfig corsConfig = new CorsConfig();
+        CorsConfigurationSource corsConfigSrc = corsConfig.corsConfigSource();
+        assertNotNull(corsConfigSrc);
+    }
+
+}
