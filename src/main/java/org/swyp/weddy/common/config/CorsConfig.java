cb1900efa8e1bcb14619adba7efb9b91e2b97526
sehoon.gim <sehoongim@gmail.com>
2025-02-18 18:56:13 +0900

    fix: cors uri 목록을 환경변수로 분리한다

--- src/main/java/org/swyp/weddy/common/config/CorsConfig.java
+++ src/main/java/org/swyp/weddy/common/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package org.swyp.weddy.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,10 +11,13 @@ import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
+    @Value("${cors.uris}")
+    private String corsUris;
+
     @Bean
     CorsConfigurationSource corsConfigSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://weddy-oi983a9q6-lawit.vercel.app"));
+        corsConfig.setAllowedOrigins(Arrays.asList(corsUris.split(",")));
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
         corsConfig.setAllowCredentials(true);
