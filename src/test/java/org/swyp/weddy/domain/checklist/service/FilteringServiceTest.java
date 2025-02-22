e0e7056d8ef4ab75e4a7073d225ca3268989eb0b
sehoon.gim <sehoongim@gmail.com>
2025-02-22 19:10:11 +0900

    test: 진행상황 하나를 기준으로 필터링한 결과를 가져올 수 있다

--- src/test/java/org/swyp/weddy/domain/checklist/service/FilteringServiceTest.java
+++ src/test/java/org/swyp/weddy/domain/checklist/service/FilteringServiceTest.java
@@ -1,10 +1,13 @@
 package org.swyp.weddy.domain.checklist.service;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.swyp.weddy.domain.checklist.web.response.LargeCatItemResponse;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FilteringServiceTest {
 
@@ -16,6 +19,14 @@ class FilteringServiceTest {
         public void receive_one_status_as_filtering_condition() {
             FilteringService filteringService = new FilteringServiceImpl();
             filteringService.filterByStatus(1L, "시작전");
+        }
+
+        @DisplayName("진행상황 하나를 기준으로 필터링한 결과를 가져올 수 있다")
+        @Test
+        public void return_filtering_result_with_one_status() {
+            FilteringService filteringService = new FilteringServiceImpl();
+            List<LargeCatItemResponse> result = filteringService.filterByStatus(1L, "시작전");
+            assertThat(result).isNotNull();
         }
     }
 }
