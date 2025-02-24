16f559ac527edd13bfdd2e5d6ac0a7c5c7710174
metta0 <jiterin908@gmail.com>
2025-02-24 15:51:06 +0900

    feat: 대분류 항목이동 - 테스트를 통과하는 Controller 구현

--- src/main/java/org/swyp/weddy/domain/checklist/web/LargeCatController.java
+++ src/main/java/org/swyp/weddy/domain/checklist/web/LargeCatController.java
@@ -5,12 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.swyp.weddy.domain.checklist.service.ChecklistService;
 import org.swyp.weddy.domain.checklist.service.LargeCatService;
-import org.swyp.weddy.domain.checklist.service.dto.ChecklistDto;
-import org.swyp.weddy.domain.checklist.service.dto.LargeCatItemAssignDto;
-import org.swyp.weddy.domain.checklist.service.dto.LargeCatItemDeleteDto;
-import org.swyp.weddy.domain.checklist.service.dto.LargeCatItemEditDto;
+import org.swyp.weddy.domain.checklist.service.dto.*;
 import org.swyp.weddy.domain.checklist.web.request.LargeCatItemDeleteRequest;
 import org.swyp.weddy.domain.checklist.web.request.LargeCatItemEditRequest;
+import org.swyp.weddy.domain.checklist.web.request.LargeCatItemMoveRequest;
 import org.swyp.weddy.domain.checklist.web.request.LargeCatItemPostRequest;
 import org.swyp.weddy.domain.checklist.web.response.ChecklistResponse;
 import org.swyp.weddy.domain.checklist.web.response.LargeCatItemResponse;
@@ -89,6 +87,19 @@ public class LargeCatController {
 
         LargeCatItemDeleteDto deleteDto = LargeCatItemDeleteDto.of(checklist.getId(), request);
         largeCatService.deleteItemWithSmallItems(deleteDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/move")
+    public ResponseEntity<Void> moveItem(@RequestBody LargeCatItemMoveRequest request) {
+        String memberId = request.getMemberId();
+        ChecklistDto dto = ChecklistDto.from(memberId);
+
+        ChecklistResponse checklist = checklistService.findChecklist(dto);
+
+        LargeCatItemMoveDto moveDto = LargeCatItemMoveDto.of(checklist.getId(), request);
+        largeCatService.moveItem(moveDto);
 
         return ResponseEntity.ok().build();
     }
