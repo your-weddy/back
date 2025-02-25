3fa6e84495098a7243af6c35a2694dae6b3fb506
sehoon.gim <sehoongim@gmail.com>
2025-02-25 16:06:26 +0900

    test: filteringService.filterByStatus() 검색 결과가 없을 때 예외를 반환한다

--- src/test/java/org/swyp/weddy/slow/FilteringSlowTest.java
+++ src/test/java/org/swyp/weddy/slow/FilteringSlowTest.java
@@ -10,6 +10,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.swyp.weddy.domain.checklist.dao.LargeCatMapper;
 import org.swyp.weddy.domain.checklist.entity.LargeCatItem;
+import org.swyp.weddy.domain.checklist.exception.LargeCatItemNotExistsException;
+import org.swyp.weddy.domain.checklist.service.FilteringService;
+import org.swyp.weddy.domain.checklist.service.dto.FilterByStatusDto;
 
 import java.util.List;
 
@@ -17,6 +20,8 @@ import java.util.List;
 @Transactional
 @SpringBootTest
 public class FilteringSlowTest {
+    @Autowired
+    private FilteringService filteringService;
     @Autowired
     private LargeCatMapper largeCatMapper;
     @Autowired
@@ -32,5 +37,18 @@ public class FilteringSlowTest {
 
         List<LargeCatItem> noResult = largeCatMapper.selectAllItems(1L);
         Assertions.assertThat(noResult).isEmpty();
+    }
+
+    @DisplayName("filteringService.filterByStatus() 검색 결과가 없을 때 예외를 반환한다")
+    @Test
+    void throws_exception_when_no_result_in_filter_by_status() {
+        jdbcTemplate.update("""
+                update large_category_item
+                set is_deleted = true
+                """);
+
+        org.junit.jupiter.api.Assertions.assertThrows(LargeCatItemNotExistsException.class, () -> {
+            filteringService.filterByStatus(new FilterByStatusDto(1L, List.of("시작전")));
+        });
     }
 }
