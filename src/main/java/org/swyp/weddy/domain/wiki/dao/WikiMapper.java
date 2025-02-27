b36be82b54042c86336ba69092efe8ca8d2568fc
sehoon gim <sehoongim@gmail.com>
2025-02-27 14:22:35 +0900

    chore: 쓰지 않는 파일을 지운다 (#66)
    - 프로젝트 구조 결정하기 위해 초기에 테스트용으로 추가한 wiki 관련
       코드를 추가했다
     - 더 이상 필요하지 않아서 지운다

--- src/main/java/org/swyp/weddy/domain/wiki/dao/WikiMapper.java
+++
@@ -1,9 +0,0 @@
-package org.swyp.weddy.domain.wiki.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.swyp.weddy.domain.wiki.entity.Wiki;
-
-@Mapper
-public interface WikiMapper {
-    Wiki selectWiki(String title);
-}
