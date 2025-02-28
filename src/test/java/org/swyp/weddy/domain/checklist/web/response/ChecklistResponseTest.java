3bf49a9835ce03f25efa64b106a14fd691265f0c
sehoon.gim <sehoongim@gmail.com>
2025-02-28 23:56:11 +0900

    refactor: 테스트 정리한다
     - ChecklistResponse.daysBeforeWedding()의 동작을 확인하는 테스트임을 알
       수 있도록 테스트를 정리한다

--- src/test/java/org/swyp/weddy/domain/checklist/web/response/ChecklistResponseTest.java
+++ src/test/java/org/swyp/weddy/domain/checklist/web/response/ChecklistResponseTest.java
@@ -3,12 +3,8 @@ package org.swyp.weddy.domain.checklist.web.response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.swyp.weddy.domain.checklist.entity.Checklist;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,38 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ChecklistResponseTest {
 
     @DisplayName("결혼까지 남은 일자를 계산할 수 있다")
-    @Test
-    public void compute_days_left_before_wedding() {
-        Checklist checklist = TestChecklist.from();
-        LocalDate weddingDate = ChecklistResponse.weddingDate(checklist);
-        LocalDate today = LocalDate.of(2025,3,1);
-
-        Long dDayCount = ChecklistResponse.daysBeforeWedding(weddingDate, today);
-
-        assertThat(dDayCount).isInstanceOf(Long.class);
-        assertThat(dDayCount).isGreaterThan(0);
-    }
-
-    private static class TestChecklist extends Checklist {
-        static Checklist from() {
-            return new Checklist(
-                    1L,
-                    LocalDateTime.parse("2025-12-01T00:00:00"),
-                    new Timestamp(System.currentTimeMillis()),
-                    null,
-                    Boolean.FALSE
-            );
-        }
-    }
-
-    @DisplayName("두 날짜 사이 일수를 계산할 수 있다")
     @Nested
-    class DayCountComputeTest {
+    class DaysBeforeWeddingComputeTest {
         @DisplayName("결혼 당일")
         @Test
         public void when_wedding_day() {
             assertThat(
-                    ChronoUnit.DAYS.between(
+                    ChecklistResponse.daysBeforeWedding(
                             LocalDate.of(2025, 3, 1),
                             LocalDate.of(2025, 3, 1)
                     )
@@ -59,9 +30,9 @@ class ChecklistResponseTest {
         @Test
         public void one_day_before_wedding() {
             assertThat(
-                    ChronoUnit.DAYS.between(
-                            LocalDate.of(2025, 3, 1),
-                            LocalDate.of(2025, 3, 2)
+                    ChecklistResponse.daysBeforeWedding(
+                            LocalDate.of(2025, 3, 2),
+                            LocalDate.of(2025, 3, 1)
                     )
             ).isEqualTo(1);
         }
@@ -79,9 +50,9 @@ class ChecklistResponseTest {
                 Integer weddingMonth = integerList.get(1);
                 Long expected = Long.valueOf(integerList.get(2));
                 assertThat(
-                        ChronoUnit.DAYS.between(
-                                LocalDate.of(2025, todayMonth, 1),
-                                LocalDate.of(2025, weddingMonth, 1)
+                        ChecklistResponse.daysBeforeWedding(
+                                LocalDate.of(2025, weddingMonth, 1),
+                                LocalDate.of(2025, todayMonth, 1)
                         )
                 ).isEqualTo(expected);
             }
@@ -91,9 +62,9 @@ class ChecklistResponseTest {
         @Test
         public void one_year_before_wedding() {
             assertThat(
-                    ChronoUnit.DAYS.between(
-                            LocalDate.of(2025, 3, 1),
-                            LocalDate.of(2026, 3, 1)
+                    ChecklistResponse.daysBeforeWedding(
+                            LocalDate.of(2026, 3, 1),
+                            LocalDate.of(2025, 3, 1)
                     )
             ).isEqualTo(365);
         }
