ae0ffdcefd7578ec682e8911f0b8365bca967398
sehoon.gim <sehoongim@gmail.com>
2025-02-28 16:49:03 +0900

    feat: 정해진 포맷에 맞춘 결혼 예정일자 정보만 보낼 수 있다
     - 2025-12-01 과 같은 형식으로만 요청을 보낼 수 있다
     - 형식에 맞지 않으면 400 예외를 던진다

--- src/main/java/org/swyp/weddy/domain/checklist/web/request/ChecklistDdayAssignRequest.java
+++ src/main/java/org/swyp/weddy/domain/checklist/web/request/ChecklistDdayAssignRequest.java
@@ -1,8 +1,13 @@
 package org.swyp.weddy.domain.checklist.web.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 public class ChecklistDdayAssignRequest {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    @JsonProperty("dDay")
     private LocalDate dDay;
 
     public ChecklistDdayAssignRequest() {
