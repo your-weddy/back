a794ac71087488dfb134432edd71b69e7425ecbc
sehoon.gim <sehoongim@gmail.com>
2025-02-28 17:15:08 +0900

    feat: 결혼 예정일 등록 요청을 받을 수 있다

--- src/main/java/org/swyp/weddy/domain/checklist/service/dto/ChecklistDdayAssignDto.java
+++ src/main/java/org/swyp/weddy/domain/checklist/service/dto/ChecklistDdayAssignDto.java
@@ -2,7 +2,17 @@ package org.swyp.weddy.domain.checklist.service.dto;
 
 import org.swyp.weddy.domain.checklist.web.request.ChecklistDdayAssignRequest;
 
+import java.time.LocalDate;
+
 public class ChecklistDdayAssignDto {
+    private String memberId;
+    private LocalDate dDay;
+
+    public ChecklistDdayAssignDto(String memberId, LocalDate dDay) {
+        this.memberId = memberId;
+        this.dDay = dDay;
+    }
+
     public static ChecklistDdayAssignDto from(String memberId, ChecklistDdayAssignRequest request) {
         return null;
     }
