f659d8b7bd0a725ff038ef5e604b6c9675ef17f0
sehoon.gim <sehoongim@gmail.com>
2025-02-28 16:33:23 +0900

    feat: 요청은 날짜 정보를 포함한다

--- src/main/java/org/swyp/weddy/domain/checklist/web/request/ChecklistDdayAssignRequest.java
+++ src/main/java/org/swyp/weddy/domain/checklist/web/request/ChecklistDdayAssignRequest.java
@@ -1,4 +1,18 @@
 package org.swyp.weddy.domain.checklist.web.request;
 
+import java.time.LocalDate;
+
 public class ChecklistDdayAssignRequest {
+    private LocalDate dDay;
+
+    public ChecklistDdayAssignRequest() {
+    }
+
+    public ChecklistDdayAssignRequest(LocalDate dDay) {
+        this.dDay = dDay;
+    }
+
+    public LocalDate getdDay() {
+        return dDay;
+    }
 }
