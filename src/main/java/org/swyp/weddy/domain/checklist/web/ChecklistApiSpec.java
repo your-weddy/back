d078bb29972bdba62f2e1545cd4c001cdec360c8
sehoon.gim <sehoongim@gmail.com>
2025-03-02 00:11:49 +0900

    doc: 체크리스트 생성 API 수정 사항을 swagger 문서에 반영한다

--- src/main/java/org/swyp/weddy/domain/checklist/web/ChecklistApiSpec.java
+++ src/main/java/org/swyp/weddy/domain/checklist/web/ChecklistApiSpec.java
@@ -2,19 +2,16 @@ package org.swyp.weddy.domain.checklist.web;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.Explode;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.enums.ParameterStyle;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.swyp.weddy.domain.checklist.web.request.ChecklistCreateRequest;
 import org.swyp.weddy.domain.checklist.web.response.ChecklistResponse;
-
-import java.util.Map;
 
 public interface ChecklistApiSpec {
 
@@ -22,17 +19,17 @@ public interface ChecklistApiSpec {
     @Operation(summary = "체크리스트 생성", description = "새로운 체크리스트를 만든다")
     @PostMapping
     ResponseEntity<Void> createChecklist(
-            @Parameter(
-                    name = "memberIdMap",
-                    description = "사용자 아이디를 갖는 key-value 형식 값",
-                    in = ParameterIn.QUERY,
-                    required = true,
-                    schema = @Schema(type = "object"),
-                    style = ParameterStyle.FORM,
-                    explode = Explode.TRUE,
-                    example = "{ \"memberId\": \"2\" }"
-            )
-            @RequestBody Map<String, String> memberIdMap
+            @Parameter(name = "request", description = "새로운 체크리스트를 만들어줄 사용자 아이디", required = true)
+            @RequestBody(content = @Content(
+                    examples = {
+                            @ExampleObject(name = "someExample1", value = """
+                                        {
+                                            "memberId" : "1"
+                                        }
+                                    """)
+                    }
+            ))
+            @org.springframework.web.bind.annotation.RequestBody ChecklistCreateRequest request
     );
 
     @Tag(name = "checklist", description = "체크리스트 관련 API")
