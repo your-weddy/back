7ca63ec9f1ea4cc22e3c6ffc1181a5e13dc7ea62
metta0 <jiterin908@gmail.com>
2025-03-01 16:56:56 +0900

    test: 사용자 프로필 이미지 변경 응답에 성공할 수 있다

--- src/test/java/org/swyp/weddy/domain/member/web/MemberControllerTest.java
+++ src/test/java/org/swyp/weddy/domain/member/web/MemberControllerTest.java
@@ -1,20 +1,21 @@
 package org.swyp.weddy.domain.member.web;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.swyp.weddy.domain.member.service.MemberService;
 
 import static org.mockito.Mockito.mock;
 
 class MemberControllerTest {
 
+    Long memberId;
+    String profileImgUrl;
     MemberService memberService;
     MemberController memberController;
 
     @BeforeEach
     void set_up(){
+        Long memberId = 1L;
+        String profileImgUrl = "test.img";
         memberService = mock(MemberService.class);
         memberController = new MemberController(memberService);
     }
@@ -25,12 +26,18 @@ class MemberControllerTest {
         @DisplayName("사용자 프로필 이미지 업로드 요청을 받을 수 있다.")
         @Test
         void get_user_profile_image_request(){
-            //given
-            Long memberId = 1L;
-            String profileImgUrl = "test.img";
-
             //when, then
             memberController.updateProfileImageUrl(memberId, profileImgUrl);
+        }
+
+        @DisplayName("사용자 프로필 이미지 업로드를 성공 할 수 있다.")
+        @Test
+        void success_user_profile_image_request(){
+            //when
+            var result = memberController.updateProfileImageUrl(memberId, profileImgUrl);
+
+            //then
+            Assertions.assertEquals(200, result.getStatusCodeValue());
         }
     }
 
